b471ce61f7135265dbffa8c036de1dfa331fe708
Sakshi kumari 201 <sakshiparmarsingh456@gmail.com>
2026-04-29 12:03:42 +0530

    Fix Express 5 wildcard routing error

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use('/api/user',userRouter)
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 const PORT=process.env.PORT || 3000
