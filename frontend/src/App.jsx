3e5dd3b81d7fc748e95fe0295534bf878529e650
Sakshi kumari 201 <sakshiparmarsingh456@gmail.com>
2026-04-29 12:38:53 +0530

    Redirect authenticated users away from login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,21 @@
 import Login from "./login/Login.jsx"
 import { ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { Route ,Routes } from "react-router-dom";
+import { Route ,Routes, Navigate } from "react-router-dom";
 import Register from "./register/Register.jsx";
 import Home from "./home/Home.jsx";
 import { VerifyUser } from "./utils/VerifyUser.jsx";
+import { useAuth } from "./context/AuthContext";
+
 function App() {
+  const { authUser } = useAuth();
   
   return (
     <>
     <div className="p-2 w-screen h-screen flex items-center justify-center">
       <Routes>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/register" element={<Register/>}/>
+        <Route path="/login" element={authUser ? <Navigate to="/" /> : <Login/>}/>
+        <Route path="/register" element={authUser ? <Navigate to="/" /> : <Register/>}/>
         <Route element={<VerifyUser/>}>
         <Route path="/" element={<Home/>}/>
         </Route>
